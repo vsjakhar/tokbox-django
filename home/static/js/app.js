e1b5c18b14d7cc14599304e8490ba84068234c22
Jyoti <vijayjakhar.vj@gmail.com>
2019-08-02 18:03:37 +0530

    Tokbox js and css setup with api key and token

---
+++ home/static/js/app.js
@@ -0,0 +1,44 @@
+// replace these values with those generated in your TokBox Account
+var apiKey = "YOUR_API_KEY";
+var sessionId = "YOUR_SESSION_ID";
+var token = "YOUR_TOKEN";
+
+// Handling all of our errors here by alerting them
+function handleError(error) {
+  if (error) {
+    alert(error.message);
+  }
+}
+
+// (optional) add server code here
+initializeSession();
+
+function initializeSession() {
+  var session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', function(event) {
+    session.subscribe(event.stream, 'subscriber', {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    }, handleError);
+  });
+
+  // Create a publisher
+  var publisher = OT.initPublisher('publisher', {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  }, handleError);
+
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, initialize a publisher and publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
+}
